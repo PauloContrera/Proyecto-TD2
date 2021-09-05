b7c8adaea9509b58e4600752ec9742015c29033c
PauloContrera <paulocontrera97@gmail.com>
2021-09-05 19:53:47 -0300

    Base

---
+++ esp8266firmware/commands.lua
@@ -0,0 +1,6 @@
+wifi.setmode(wifi.STATION)
+print('set mode=STATION (mode='..wifi.getmode()..')')
+print('MAC: ',wifi.sta.getmac())
+print('chip: ',node.chipid())
+print('heap: ',node.heap())
+wifi.sta.config("Nombre_de_AP","Contraseña_de_AP")
