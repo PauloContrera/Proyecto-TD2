55b0f8f7e573d2aa77fe84d64aa42ac166c51b39
PauloContrera <paulocontrera97@gmail.com>
2021-09-30 21:15:53 -0300

    PIC

---
+++ Proteus/Pic/desde-cero/Pruebas/pic/auxi/main.h
@@ -0,0 +1,23 @@
+#include <18F2550.h>
+#device ADC=10
+
+#FUSES NOWDT                    //No Watch Dog Timer
+
+#use delay(internal=4MHz)
+
+
+/*
+#include <main.h>
+
+void main()
+{
+
+   while(TRUE)
+   {
+
+
+      //TODO: User Code
+   }
+
+}
+*/
